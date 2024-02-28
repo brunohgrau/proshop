ec1105173918ebcc5580e46850d9ab0a08e65162
Bruno <brunohgrau99@gmail.com>
2024-02-28 20:23:29 -0300

    Custom Error Middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 
 connectDB(); // Connect to MongoDB
@@ -15,5 +16,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is runing on port ${port}`));
